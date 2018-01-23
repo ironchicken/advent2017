0f7ff8e47ffd1051f1a6a2af871a39f0ded1e4c1
Richard Lewis <richard@rjlewis.me.uk>
2018-01-23 23:08:49 +0000

    Finished 01 part 2

--- 01/InverseCaptcha.hs
+++ 01/InverseCaptcha.hs
@@ -1,15 +1,9 @@
 module InverseCaptcha where
 
-forwardRepetitions :: (Eq a) => [a] -> [a]
-forwardRepetitions xs = fr (head xs) xs
-  where
-    fr hd (a1:a2:as)
-      | a2 == a1 = a2 : fr hd (a2 : as)
-      | otherwise = fr hd (a2 : as)
-    fr hd [a]
-      | a == hd = [a]
-      | otherwise = []
-    fr _ [] = []
+circulate :: Int -> [a] -> [a]
+circulate i xs =
+  let i' = i `mod` length xs
+  in drop i' xs ++ take i' xs
 
 digitsToNumbers :: String -> [Int]
 digitsToNumbers = map read . map (\c -> [c])
@@ -17,5 +11,8 @@ digitsToNumbers = map read . map (\c -> [c])
 main :: IO ()
 main = do
   input <- readFile "input"
-  let answer = sum . forwardRepetitions . digitsToNumbers $ input
-  putStrLn $ show answer
+  let inputDigits = digitsToNumbers input
+      answerP1 = sum $ map fst $ filter (\(a, b) -> a == b) $ zip inputDigits $ circulate 1 inputDigits
+      answerP2 = sum $ map fst $ filter (\(a, b) -> a == b) $ zip inputDigits $ circulate (length inputDigits `div` 2) inputDigits
+  putStrLn $ show answerP1
+  putStrLn $ show answerP2
