6695980fdc4f32867668cf97bd7b3543809e8b4c
Richard Lewis <richard@rjlewis.me.uk>
2018-02-09 23:47:02 +0000

    Finished 04 part 2

--- 04/HighEntryPassphrases.hs
+++ 04/HighEntryPassphrases.hs
@@ -1,11 +1,14 @@
 module HighEntryPassphrases where
 
-import Data.List (nub)
+import Data.List (nub, sort)
 
 main :: IO ()
 main = do
   passphraseFile <- readFile "input"
   let phrases = lines passphraseFile
       noDuplicates str = (length $ words str) == (length $ nub $ words str)
+      noAnagrams str = (length $ words str) == (length $ nub $ map sort $ words str)
       answerP1 = length $ filter noDuplicates phrases
+      answerP2 = length $ filter noAnagrams phrases
   putStrLn $ show answerP1
+  putStrLn $ show answerP2
