af6c636395a3c1d5d113fa340302ff6f8961d1d8
Richard Lewis <richard@rjlewis.me.uk>
2018-02-19 08:30:30 +0000

    Finished 05 part 2

--- 05/JumpMaze.hs
+++ 05/JumpMaze.hs
@@ -1,18 +1,32 @@
+{-# LANGUAGE BangPatterns #-}
+
 module JumpMaze where
 
 import Data.Map (Map)
 import qualified Data.Map as M
 
-jump :: Int -> Int -> Map Int Int -> Int
-jump counter position instructions =
+jump :: Integer -> Integer -> (Integer -> Maybe Integer) -> Map Integer Integer -> Integer
+jump counter position mutateOffset instructions =
   doJump $ M.lookup position instructions
   where
-    doJump (Just jmp) = jump (counter + 1) (position + jmp) $ M.update (\j -> Just $ j + 1) position instructions
+    doJump (Just !jmp) = jump nextCounter nextPosition mutateOffset $! M.update mutateOffset position instructions
+      where
+        !nextCounter = counter + 1
+        !nextPosition = position + jmp
     doJump Nothing = counter
+
 
 main :: IO ()
 main = do
   input <- readFile "input"
   let instructions = M.fromList $ zip [0..] (map read $ lines input)
-      answerP1 = jump 0 0 instructions
+      p1MutateOffset offs = Just $ offs + 1
+      answerP1 = jump 0 0 p1MutateOffset instructions
+
+      p2MutateOffset offs
+        | offs >= 3 = Just $ offs - 1
+        | otherwise = Just $ offs + 1
+      answerP2 = jump 0 0 p2MutateOffset instructions
+
   putStrLn $ show answerP1
+  putStrLn $ show answerP2
